03e1af7de916d1d9693a5934f2feb2953cc33c84
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-02 17:35:01 +0900

    TEST | 정적파일 제외하면 실행되는지 확인

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
 
 const app = express();
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.send("안녕하세요");
+})
 
 app.listen(3000, () => {
   console.log("Amplify Test");
